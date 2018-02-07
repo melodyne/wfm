2feab5c302b51824b21ebf4c09cfceb889d7994c
陈万洲 <295124540@qq.com>
2018-02-07 13:32:08 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+	$dirPath ='../../runtime/log';
+	if(isset($_GET['dir']))$dirPath = $_GET['dir'];
+	$action = null;
+
+	//引入文件
+	require "./onlineEditor.php";
+
+	//获得onlineEditor对象
+	$onlineEditor = new onlineEditor($dirPath);
+	$fileMes = $onlineEditor->main();
+
+	//处理文件路径
+	function subFilePath($dirPath,$filename){
+		// echo $dirPath . $filename;
+		return $dirPath . $filename;
+	}
+
+	//初始化
+	if(array_key_exists('action', $_GET)){
+		switch ($_GET['action']) {
+			case 'open':
+				$action = 'open';
+				break;
+            case 'look':
+                $action = 'look';
+                break;
+            case 'updata':
+                $action = 'updata';
+                break;
+			case 'del':
+				$onlineEditor->delFile(subFilePath($dirPath,$_GET['filename']));
+				$action = 'del';
+				echo subFilePath($dirPath,$_GET['filename']);
+				echo "<script>location.href = 'index.php';</script>";
+				break;
+		}
+	} else{
+		$action = null;
+	}
+
+	if(array_key_exists('action', $_POST)){
+		switch ($_POST['action']) {
+			case 'create':
+				$onlineEditor->createFile(subFilePath($dirPath,$_POST['filename']));
+				echo "<script>location.href = 'index.php';</script>";
+				break;
+		}
+	}
+
+	//获取文件内容
+	if(array_key_exists('filename', $_GET) && ($_GET['action'] == 'updata'||$_GET['action'] == 'look')){
+		$root = subFilePath(rtrim($dirPath,'/').'/',$_GET['filename']);
+		$fileContent = $onlineEditor -> getContent($root);
+		//echo "<pre style='font-size: 16px'>$fileContent</pre>";die();
+	} else
+		$fileContent = "坚持就是胜利";
+
+	if (array_key_exists('filecontent', $_POST)) {
+		// var_dump($_POST);
+		$onlineEditor->putContent(subFilePath($dirPath,$_POST['filename']),$_POST['filecontent']);
+		echo "<script>location.href = 'index.php';</script>";
+	}	
+
+
+	//引入界面
+	require "./viewEditor.html";
+
+	// print_r($fileMes);
+
+?>
---
+++ onlineEditor.php
@@ -0,0 +1,141 @@
+<?php
+
+//在线文件编辑器
+/*-------------------------------------
+   使用工厂设计模式，MVC实现
+*/
+
+class onlineEditor{
+
+	//设置全局变量路径
+	public $filePath = null;
+	//设置过滤信息
+	private $fileFilter = array(
+		'onlineEditor.php',
+		'viewEditor.html',
+		'index.php',
+		'.',
+		'..',
+        '.svn'
+	);
+	
+	//构造函数必须是私有的在单例设计模式中
+	function __construct($filePath){
+		$this->filePath = $filePath;
+	}
+
+	//当本类销毁的时候进行的操作
+	function __destruct(){
+		// echo $this->filePath;
+	}
+
+	//获取文件的内容
+	function getContent($filePath){
+		if (!isset($filePath)) {
+			
+		} else{
+            if(filetype($filePath)=='file'){
+                $fileContent = file_get_contents($filePath);
+                return $fileContent;
+            }
+		}
+	}
+
+	//放入文件内容
+	function putContent($filePath,$fileContent){
+		file_put_contents($filePath, $fileContent);
+	}
+
+	//判断目录是否存在
+	private function judgeExist(){
+		//判断目录是否为空或者没有文件
+		if(is_dir($this->filePath) && file_exists($this->filePath)){
+			return true;
+		} else{
+			return false;
+		}
+	}
+
+	//创建文件
+	function createFile($filename){
+		if(!file_exists($filename)){
+			fopen($filename, "w+");
+		}
+			
+		else{
+			echo "<a href = 'index.php' >点此返回</a>";
+		 	die("文件已经存在");
+		}
+			
+	}
+	//删除文件
+	function delFile($filename){
+		if(file_exists($filename)){
+			unlink($filename);
+		}
+	}
+
+	// 文件排序
+    function my_sort($arrays,$sort_key,$sort_order=SORT_ASC,$sort_type=SORT_NUMERIC ){
+	    if($arrays==[])return [];
+        if(!is_array($arrays))return false;
+
+        foreach ($arrays as $array){
+            if(is_array($array)){
+                $key_arrays[] = $array[$sort_key];
+            }else{
+                return false;
+            }
+        }
+        array_multisort($key_arrays,$sort_order,$sort_type,$arrays);
+        return $arrays;
+    }
+
+    //主函数
+	function main(){
+		if($this->judgeExist()){
+			//获取打开文件夹对象
+			$fileOpen = opendir($this->filePath);
+            $dirArr = array();
+			$fileArr = array();
+			$i = 1;
+			//遍历文件夹
+			while ($file = readdir($fileOpen)) {
+				//过滤
+				if(in_array($file, $this->fileFilter)){
+					continue;
+				}
+                $path = rtrim($this->filePath,'/').'/'.$file;
+				$type = fileType($path);
+				if($type=='dir'){ // 文件夹
+                    $dirArr[] = array(
+                        'fileCode'  => $i,
+                        'fileName'  => $file,
+                        'fileType'  => $type,
+                        'fileSize'  => fileSize($path),
+                        'filemtime' => filemtime($path)
+                    );
+                }else{ // 文件
+                    $fileArr[] = array(
+                        'fileCode'  => $i,
+                        'fileName'  => $file,
+                        'fileType'  => $type,
+                        'fileSize'  => fileSize($path),
+                        'filemtime' => filemtime($path)
+                    );
+                }
+		        $i++;
+		    }
+            $dirArr = $this->my_sort($dirArr,'fileName',SORT_ASC,SORT_STRING);
+            $fileArr = $this->my_sort($fileArr,'fileName',SORT_ASC,SORT_STRING);
+            closedir($fileOpen); //关闭文件
+            return array_merge($dirArr,$fileArr);
+
+        } else{
+			die("不存在此文件夹");
+		}
+	}
+
+}
+
+?>
